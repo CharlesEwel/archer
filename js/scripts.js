4b22837e06e958d8136399374f8a1152c3a58436
Alex Jacks and Charles Ewel <charles@>
2016-06-02 15:51:25 -0700

    fixed logic behind form input; nine possible results now work

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,53 @@
+
+var extractViolence = function(rawscore) {
+  return parseInt(String(rawscore.charAt(0)))-5;
+};
+var extractWildness = function(rawscore) {
+  return parseInt(String(rawscore.charAt(1)))-5;
+};
+
 $(document).ready(function() {
-  $("form#politics").submit(function(event) {
+  $("form#archer").submit(function(event) {
     event.preventDefault();
 
-    var politics = 0;
-      politics += parseInt($("#Punishment").val());
-      politics += parseInt($("#Electoral").val());
-      politics += parseInt($("#Finance").val());
-      politics += parseInt($("#Security").val());
-      politics += parseInt($("#Environmental").val());
+    var violentness = 0;
+    violentness += extractViolence($("#blimp").val());
+    violentness += extractViolence($("#Midnight").val());
+    violentness += extractViolence($("#mortal").val());
+    violentness += extractViolence($("#kidnap").val());
+    violentness += extractViolence($("#yakuza").val());
 
-    if(politics >= 5){
-      $('#stand').text('Quite Liberal');
-    } else if (politics >= 1){
-      $('#stand').text('Liberal');
-    } else if (politics >= -1){
-      $('#stand').text('Politically Flexible');
-    } else if (politics >= -5){
-      $('#stand').text('Conservative');
+    var wildness = 0;
+    wildness += extractWildness($("#blimp").val());
+    wildness += extractWildness($("#Midnight").val());
+    wildness += extractWildness($("#mortal").val());
+    wildness += extractWildness($("#kidnap").val());
+    wildness += extractWildness($("#yakuza").val());
+
+    if(violentness >= 12){
+      if(wildness >= 12){
+        $('#character').text('Sterling Archer');
+      } else if (wildness > -12 ){
+        $('#character').text('Pam Poovey');
+      } else {
+        $('#character').text('Lana Kane');
+      }
+    } else if (violentness > -12){
+      if(wildness >= 12){
+        $('#character').text('Cheryl Tunt');
+      } else if (wildness > -12){
+        $('#character').text('Ron Cadillac');
+      } else {
+        $('#character').text('Ray Gillette');
+      }
     } else {
-      $('#stand').text('Quite Conservative');
+      if(wildness >= 12){
+        $('#character').text('Dr. Krieger');
+      } else if (wildness > -12){
+        $('#character').text('Mallory Archer');
+      } else {
+        $('#character').text('Cyril Figgis');
+      }
     }
 
     $('#output').show();
